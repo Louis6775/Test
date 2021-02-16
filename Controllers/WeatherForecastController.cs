009da8b178c8ee819841446cbebfc86f21d6ebe0
louis.ribault <louis.ribault@epita.fr>
2021-02-16 15:12:46 +0000

    Second push

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace Testapplication.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
-                Hello = "Hello mate how you doin"
+                Hello = "Hello mate how you doin mateyyyy"
             })
             .ToArray();
         }
